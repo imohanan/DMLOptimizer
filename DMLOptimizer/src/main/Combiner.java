7a90a723718eb919b10ffc3f5ac30af551a5d529
imohanan <indu.mohanan4@gmail.com>
2016-03-15 19:48:24 -0700

    Adding dmls to to FK and PK Maps

--- DMLOptimizer/src/main/Combiner.java
+++ DMLOptimizer/src/main/Combiner.java
@@ -1,10 +1,12 @@
 package main;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Vector;
 
 import model.DML;
+import model.FKValue;
 
 public class Combiner 
 {
@@ -14,12 +16,54 @@ public class Combiner
 	
 	public static void addDML(DML dml)
 	{
-		// ToDo
-		// 1.add to PKValuesMap
-		// 2. add to FKValuesMap
+		dml.SetPrimaryKeyValue();
+    	dml.SetForeignKeyValues();
+    	
+		// 1.add to PKValuesMap 
+    	// 1a. get table
+    	HashMap<String, List<DML>> tableHashMap = PKValuesMap.get(dml.table);
+    	if (tableHashMap == null)
+    	{
+    		PKValuesMap.put(dml.table, new HashMap<String, List<DML>>() );
+    		tableHashMap = PKValuesMap.get(dml.table);
+    	}
+    	
+    	// 1b.add DML to PK 
+    	List<DML> recordDMLs = tableHashMap.get(dml.PKValue);
+    	if (recordDMLs == null)
+    	{
+    		recordDMLs = new LinkedList<DML>();
+    	}
+    	recordDMLs.add(dml);
+		tableHashMap.put(dml.PKValue, recordDMLs);
+		
+		// 2. Add to FKValuesMap
+		for(FKValue fkValue: dml.FKValues)
+		{
+			// 2a. get table
+			HashMap<String, List<DML>> fkTableHashMap = FKValuesMap.get(fkValue.Referenced_Table);
+			if (fkTableHashMap == null)
+			{
+				FKValuesMap.put(fkValue.Referenced_Table, new HashMap<String, List<DML>>());
+				fkTableHashMap = FKValuesMap.get(fkValue.Referenced_Table);
+			}
+			
+			//2. add DML to FK
+			List<DML> fkRecordDMLs = fkTableHashMap.get(fkValue.FKValueString);
+			if (fkRecordDMLs == null)
+	    	{
+				fkRecordDMLs = new LinkedList<DML>();
+	    	}
+			fkRecordDMLs.add(dml);
+			fkTableHashMap.put(fkValue.FKValueString, fkRecordDMLs);
+		}		
 	}
 	
 	
+	public static void ApplyOptimizerRules(DML dml)
+	{
+		
+	}
 
 	public static List<DML> GetRecordLevelDMLs(DML dml) {
 		// TODO Auto-generated method stub
