78ac6cf095aa312945bf8adfabcce3fd6545fd68
imohanan <indu.mohanan4@gmail.com>
2016-03-16 13:14:16 -0700

    Added DMlQueue methods required by batch

--- DMLOptimizer/src/model/DMLQueue.java
+++ DMLOptimizer/src/model/DMLQueue.java
@@ -67,5 +67,20 @@ public class DMLQueue {
 		dml.PrevNode = null;
 	}
 	
-
+	
+	public static void AddToHead(DML dml)
+	{
+		
+	}
+	
+	
+	public static DML RemoveFromHead(DML dml)
+	{
+		return null;
+	}
+	
+	public static Boolean IsQueueEmpty()
+	{
+		return false;
+	}
 }
